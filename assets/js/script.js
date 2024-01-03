57600c34f58906880117d732fa574549a39862fe
C A <chadandari@outlook.com>
2024-01-03 13:39:30 -0500

    streaming service api not yet working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var url = "";
+var posterUrl = "";
 var search = "";
 function omdbTest (){
     var link = "https://www.omdbapi.com/?apikey=17b8058a&t=" + search;
@@ -15,18 +15,55 @@ function omdbTest (){
             $('#returned-title').text(returned.Title);
             $('#returned-title').attr("style","visibility:visible;");
             console.log(returned.Poster);
-            url = returned.Poster;
-            $('.returned').attr("src", url);
+            posterUrl = returned.Poster;
+            $('.returned').attr("src", posterUrl);
             $('.returned').attr("style", "visibility:visible;");
         }
     })
 }
 
+async function streamingServicesTest(title){
+    var str = title;
+    var strArray = str.split(/(\s+)/);
+    for (var i = 0; i<strArray.length; i++){
+        if (strArray[i] == " ")
+        strArray[i] = '%20';
+    }
+
+    console.log(strArray.join(""));
+    str = strArray.join("");
+    var link = "https://streaming-availability.p.rapidapi.com/search/title?title=" + str + '&country=us&show_type=all&output_language=en';
+    
+    const url = link;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'f00aaee43fmsha67ab85a8c221c8p19a26cjsn5127edbb2a6b',
+            'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        var resultArray = result[0];
+        console.log(resultArray);
+
+
+
+        // for (var i = 0; i<.length; i++){
+
+        // }
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 $('#first_name2').bind("enterKey",function(e){
     e.preventDefault();
     search = $('input').val();
     omdbTest(search);
+    streamingServicesTest(search);
     $('input').val("");
  });
 
